bff72c6abb43274108cd0215a0f9d55350b48932
radixdt2691 <radixdt.2691@gmail.com>
2023-06-09 12:47:20 +0530

    Changes inside EditUser.jsx added image update functionality and fixed gender radio selected issue

--- src/Components/EditUser.jsx
+++ src/Components/EditUser.jsx
@@ -1,15 +1,12 @@
-import React from 'react'
+import React from "react";
 import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useRef } from "react";
 import { useState } from "react";
-import { toast } from 'react-toastify';
-
-
+import { toast } from "react-toastify";
 
 export const EditUser = () => {
-
   const Name = useRef("");
   const Email = useRef("");
   const Phone = useRef("");
@@ -22,10 +19,9 @@ export const EditUser = () => {
   const female = useRef();
   const Website = useRef("");
   const Image = useRef("");
+  const [imageUrl, setImage] = useState(null);
   // const options = useMemo(() => countryList().getData(), []);
   // const [setValue] = useState("");
-
-
 
   const { id } = useParams();
   const navigate = useNavigate();
@@ -41,9 +37,10 @@ export const EditUser = () => {
       State.current.value = response.data.state;
       City.current.value = response.data.city;
       Website.current.value = response.data.Website;
-      Image.current.value = response.data.imageUrl;
+      localStorage.setItem("Gender",response.data.Gender);
+      localStorage.setItem("image", response.data.imageUrl);
     });
-  }, );
+  });
 
   const algorithm = [
     {
@@ -174,7 +171,9 @@ export const EditUser = () => {
   // };
 
   const updateEmployee = () => {
-    const selectedRadio = male.current.checked ? male.current.value : female.current.value
+    const selectedRadio = male.current.checked
+      ? male.current.value
+      : female.current.value;
 
     var payload = {
       Name: Name.current.value,
@@ -187,158 +186,202 @@ export const EditUser = () => {
       city: City.current.value,
       Gender: selectedRadio,
       Website: Website.current.value,
-      Image: Image.current.value
+      imageUrl,
     };
- 
-      
-    axios.put(`http://localhost:5000/employee/${id}`, payload).then((response) => {})
+
+    axios
+      .put(`http://localhost:5000/employee/${id}`, payload)
+      .then((response) => {});
     navigate("/all");
   };
-
   const notify = () => {
-    toast.success("Updated Successfully!!",{position:toast.POSITION.TOP_CENTER});
+    toast.success("Updated Successfully!!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
   };
- 
+
+  const onImageChange = (event) => {
+    const { files } = document.querySelector('input[type="file"]');
+    const formData = new FormData();
+    formData.append("file", files[0]);
+    // replace this with your upload preset name
+    formData.append("upload_preset", "vownweha");
+    formData.append("folder", "user_profiles");
+    const options = {
+      method: "POST",
+      body: formData,
+    };
+
+    // replace cloudname with your Cloudinary cloud_name
+    return fetch(
+      "https://api.Cloudinary.com/v1_1/drmykupvw/image/upload",
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setImage(res.secure_url);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const imgSrc = localStorage.getItem("image");
+  const Gender = localStorage.getItem("Gender");
   return (
-
     <div className="container mx-auto">
-    <h1 className="text-center">Edit</h1>
-    <form
-      method="post"
-      className="col-5 d-flex flex-column gap-3 justify-center mx-auto"
-    >
-      <div class="form-group">
-        <label htmlFor="Image">Image</label>
-        <input type="url" class="form-control" id="Image" ref={Image} />
-      </div>
-      <div class="form-group">
-        <label htmlFor="Name">Name</label>
-        <input type="text" class="form-control" id="Name" ref={Name} />
-      </div>
-      <div class="form-group">
-        <label htmlFor="Email">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="Email"
-          placeholder="name@example.com"
-          ref={Email}
-        />
-      </div>
-      <div class="form-group">
-        <label id="number-label" htmlFor="number">
-          Phone Number
-        </label>
-        <input type="text" id='number' className='form-control' ref={Phone}/>
-      
-      </div>
-      <div class="form-group">
-        <label htmlFor="Bio">Bio</label>
-        <input type="text" class="form-control" id="Bio" ref={Bio} />
-      </div>
-      <div class="form-group d-flex flex-column">
-        <label htmlFor="Address">Address</label>
-        <textarea
-          className="form-control"
-          name="address"
-          id="Address"
-          cols="30"
-          rows="5"
-          ref={Address}
-        ></textarea>
-      </div>
-
-      <div class="form-group">
-        <div className="d-flex justify-content-center gap-3">
-          <div className="col-3">
-            <label htmlFor="Zip">Zip-Code</label>
-            <input type="text" class="form-control" id="Zip" ref={Zip} />
-          </div>
-          <div className="col-3">
-            <label htmlFor="State">State</label>
-            <select
-              ref={State}
-              value={algorithm.value}
-              className="form-control"
-              onChange={(e) => setSelected(e.target.value)}
-            >
-              {algorithm.map(({ value, label }, index) => (
-                <option value={value}>{label}</option>
-              ))}
-            </select>
-          </div>
-          <div className="col-3">
-            <label htmlFor="City">City</label>
-            {/* <Select options={type} ref={City}></Select> */}
-            <select ref={City} value={type.value} className="form-control">
-              {type.map(({ value, label }, index) => (
-                <option value={value}>{label}</option>
-              ))}
-            </select>
+      <h1 className="text-center">Edit</h1>
+      <form
+        method="post"
+        className="col-5 d-flex flex-column gap-3 justify-center mx-auto"
+      >
+        <div className="form-group">
+          <label htmlFor="Image">Image</label>
+          <input
+            type="file"
+            id="Image"
+            className="form-control mb-3"
+            onChange={onImageChange}
+            ref={Image}
+          />
+          <div className="d-flex gap-2">
+            <div className="d-flex flex-column">
+              <p className="h6 text-success">New</p>
+              <img src={imageUrl} className="col-5 rounded-2"/>
+            </div>
+            <div className="d-flex flex-column">
+              <p className="h6 text-danger">Current</p>
+              <img src={imgSrc} className="col-5 rounded-2"/>
+            </div>
           </div>
         </div>
-      </div>
+        <div class="form-group">
+          <label htmlFor="Name">Name</label>
+          <input type="text" class="form-control" id="Name" ref={Name} />
+        </div>
+        <div class="form-group">
+          <label htmlFor="Email">Email address</label>
+          <input
+            type="email"
+            class="form-control"
+            id="Email"
+            placeholder="name@example.com"
+            ref={Email}
+          />
+        </div>
+        <div class="form-group">
+          <label id="number-label" htmlFor="number">
+            Phone Number
+          </label>
+          <input type="text" id="number" className="form-control" ref={Phone} />
+        </div>
+        <div class="form-group">
+          <label htmlFor="Bio">Bio</label>
+          <input type="text" class="form-control" id="Bio" ref={Bio} />
+        </div>
+        <div class="form-group d-flex flex-column">
+          <label htmlFor="Address">Address</label>
+          <textarea
+            className="form-control"
+            name="address"
+            id="Address"
+            cols="30"
+            rows="5"
+            ref={Address}
+          ></textarea>
+        </div>
 
-      <div className="form-group">
-        <div className="d-flex justify-content-center align-items-center gap-4">
-          <div class="form-check">
-            <input
-              class="form-check-input"
-              type="radio"
-              name="flexRadioDefault"
-              id="flexRadioDefault1"
-              value="Male"
-              // checked={this.Gender === "Male"}
-              ref={male}
-            />
-            <label class="form-check-label" for="flexRadioDefault1">
-              Male
-            </label>
-          </div>
-          <div class="form-check">
-            <input
-              class="form-check-input"
-              type="radio"
-              name="flexRadioDefault"
-              id="flexRadioDefault2"
-              value="Female"
-              // checked={this.Gender === "Female"}
-              ref={female}
-              
-            />
-            <label class="form-check-label" for="flexRadioDefault2">
-              Female
-            </label>
-          </div>
-          <div>
-            <h3>|</h3>
-          </div>
-          <div className="col-5 d-flex gap-2 align-items-center">
-            <label htmlFor="Website">Website</label>
-            <input
-              type="text"
-              class="form-control"
-              id="Website"
-              ref={Website}
-            />
+        <div class="form-group">
+          <div className="d-flex justify-content-center gap-3">
+            <div className="col-3">
+              <label htmlFor="Zip">Zip-Code</label>
+              <input type="text" class="form-control" id="Zip" ref={Zip} />
+            </div>
+            <div className="col-3">
+              <label htmlFor="State">State</label>
+              <select
+                ref={State}
+                value={algorithm.value}
+                className="form-control"
+                onChange={(e) => setSelected(e.target.value)}
+              >
+                {algorithm.map(({ value, label }, index) => (
+                  <option value={value}>{label}</option>
+                ))}
+              </select>
+            </div>
+            <div className="col-3">
+              <label htmlFor="City">City</label>
+              {/* <Select options={type} ref={City}></Select> */}
+              <select ref={City} value={type.value} className="form-control">
+                {type.map(({ value, label }, index) => (
+                  <option value={value}>{label}</option>
+                ))}
+              </select>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* <div className="form-group col-10">
+        <div className="form-group">
+          <div className="d-flex justify-content-center align-items-center gap-4">
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault1"
+                value="Male"
+                checked={Gender === "Male"}
+                ref={male}
+              />
+              <label class="form-check-label" for="flexRadioDefault1">
+                Male
+              </label>
+            </div>
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault2"
+                value="Female"
+                checked={Gender === "Female"}
+                ref={female}
+              />
+              <label class="form-check-label" for="flexRadioDefault2">
+                Female
+              </label>
+            </div>
+            <div>
+              <h3>|</h3>
+            </div>
+            <div className="col-5 d-flex gap-2 align-items-center">
+              <label htmlFor="Website">Website</label>
+              <input
+                type="text"
+                class="form-control"
+                id="Website"
+                ref={Website}
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="form-group col-10">
           <div className="d-flex gap-2 align-items-start justify-content-center">
               <label htmlFor="img">Image</label>
               <input type="file" id="img" accept="image/*" ref={Image} onChange={handleChange} />
           </div>
       </div> */}
-      <button
-        type="submit"
-        class="btn btn-primary"
-        onClick={() => {updateEmployee();notify();}}
-      >
-        Edit Emp
-      </button>
-    </form>
-  </div>
-  )
-}
+        <button
+          type="submit"
+          class="btn btn-primary"
+          onClick={() => {
+            updateEmployee();
+            notify();
+          }}
+        >
+          Edit Emp
+        </button>
+      </form>
+    </div>
+  );
+};
